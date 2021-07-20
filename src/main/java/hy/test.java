1a36076c8c457912fad3557b8d0d289c0905c31c
hyliunain <1303784002@qq.com>
2021-07-20 22:12:04 +0800

    fix error

---
+++ src/main/java/hy/test.java
@@ -0,0 +1,9 @@
+package hy;
+
+public class test {
+    public static void main(String[] args) {
+      test1 t1 = new test1();
+      t1.te();
+    }
+}
+
---
+++ src/main/java/hy/test1.java
@@ -0,0 +1,8 @@
+package hy;
+
+public class test1 {
+    public  void te(){
+        System.out.println("hello world");
+    }
+
+}
